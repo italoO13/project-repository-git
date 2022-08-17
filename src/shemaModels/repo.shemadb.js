e4ac9ad75f4599bcfca960123e0b8209fb12a130
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-08-17 15:46:05 -0300

    feat: shema das duas coleções que serão utilizadas no projeto

---
+++ src/shemaModels/repo.shemadb.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const repoSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required:true,
+    },
+    url: {
+      type: String, 
+      required:true,
+      unique: true,
+    },
+    userId: {
+      type:String, 
+      required:true,
+    }
+  },
+  {
+    timestamps: true,
+  }
+)
+
+module.exports = mongoose.model('repo', repoSchema);
---
+++ src/shemaModels/user.shemadb.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema(
+  {
+    email: {
+      type: String, 
+      required: true, 
+      index: {
+        unique: true
+      }
+    },
+    password: {
+      type: String,
+      required:true,
+    }
+  },
+  {
+    timestamps: true,
+  }
+)
+
+module.exports = mongoose.model('User', userSchema);
