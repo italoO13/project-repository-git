68015f9bc6844c9900fffddaa20e6f49c1923c04
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-08-20 19:37:51 -0300

    refactor: lista de repositrios dinamica

--- dev-repo-web/src/components/Repositories/index.js
+++ dev-repo-web/src/components/Repositories/index.js
@@ -8,11 +8,20 @@ const Repositories = ({repo, onDelete, onNewRepo}) => {
     <h2 className="title">Repositórios</h2>
 
     <ul className="list">
-      <li className="item">
+      {
+        repo.map((rep) => <li key={rep._id} className="item">
+          <div className="info">
+            <p>{rep.name.split('/')[0]}</p>
+            <p>{rep.name.split('/')[1]}</p>
+          </div>
+          <button onClick={() => onDelete(rep.userId,rep._id)}>Apagar</button>
+        </li>)
+      }
+      {/* <li className="item">
         <p>Titulo do repo</p>
         <p>tecnologia</p>
         <button onClick={onDelete}>Apagar</button>
-      </li>
+      </li> */}
     </ul>
 
     <div className="new">
