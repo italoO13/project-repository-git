0201fb7672430291a688158fe2ace38b942642b1
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-08-20 15:39:35 -0300

    refactor: reescrito logica de limpar campo

--- dev-repo-web/src/components/Search/index.js
+++ dev-repo-web/src/components/Search/index.js
@@ -3,18 +3,23 @@ import React, {useState} from "react";
 const Search = ({onClear, onSearch}) => {
   const [ query, setQuery] = useState('')
 
+  const handleClean = () => {
+    setQuery('');
+    onClear('');
+  }
+
   return(
     <div className="search">
     <label htmlFor="query">Procurar: </label>
     <input 
-      type="search"
+      type="text"
       name="query" 
       id="query" 
       value={query}
       onChange={({target}) => setQuery(target.value)}
     />
     <button onClick={() => onSearch(query)}>Procurar</button>
-    <button onClick={onClear}>Limpar</button>
+    <button onClick={handleClean}>Limpar</button>
   </div>
   )
 }
