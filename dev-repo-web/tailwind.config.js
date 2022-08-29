bd5d77f41a6b7105958efc00376deea68baaab2d
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-08-29 18:01:29 -0300

    fix: implementado corretamente as cores personalizadas

--- dev-repo-web/tailwind.config.js
+++ dev-repo-web/tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    './public/index.html'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary':'#27dec0',
+        'secondary':'#36474F',
+      },
+    }
   },
   plugins: [],
 }
