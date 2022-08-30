be2c6de7cfb1487d7d6921177c72ab0c7b99289a
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-08-29 22:22:40 -0300

    style: estilização do header

--- dev-repo-web/src/components/Header/index.js
+++ dev-repo-web/src/components/Header/index.js
@@ -1,11 +1,23 @@
 import React from "react";
+import iconGit from '../../imgs/iconGit.png';
+import iconLogout from '../../imgs/iconLogout.png';
 
 const Header = ({onLogout}) => {
 
   return(
-    <div className="nav">
-      <h1 className="repo">Sisrepo</h1>
-      <button onClick={onLogout}>Sair</button>
+    <div className="header bg-secondary">
+      <div className="container px-5 md:px-0 m-auto flex justify-between h-[80px]">
+        <div className="logo flex items-center">
+          <div className="w-[50px] mr-4">
+            <img src={iconGit} className="w-full" alt="icone do git" />
+          </div>
+          <h1 className="repo text-white font-bold text-xl">Sistem Repo</h1>
+        </div>
+        <button className="logout flex items-center">
+            <img className="" src={iconLogout} alt="icone logout"/>
+            <p className="text-white font-bold text-xl" onClick={onLogout}>Sair</p>
+        </button>
+      </div>
     </div>
   )
 }
