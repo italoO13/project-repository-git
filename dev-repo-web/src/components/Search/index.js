62c6d788b4d9d486fc9f6ed3f8ccdca88af90752
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-08-30 09:15:54 -0300

    refactor: resolvido problemas de responsividade no mobile

--- dev-repo-web/src/components/Search/index.js
+++ dev-repo-web/src/components/Search/index.js
@@ -9,7 +9,7 @@ const Search = ({onClear, onSearch}) => {
   }
 
   return(
-    <div className="search flex max-w-6xl m-auto mt-10 items-center flex-wrap	">
+    <div className="px-2 md:px-6 search flex max-w-5xl m-auto mt-10 items-center flex-wrap	">
     <input 
       className="drop-shadow-md border py-2 px-3 flex-1
       focus:outline-none text-gray-700
@@ -21,18 +21,20 @@ const Search = ({onClear, onSearch}) => {
       value={query}
       onChange={({target}) => setQuery(target.value)}
     />
-    <button
-      onClick={() => onSearch(query)}
-      className="drop-shadow-md bg-primary text-white py-2 px-16 ml-2 rounded hover:bg-[#229F8B]"
+    <div className="m-auto">
+      <button
+        onClick={() => onSearch(query)}
+        className="drop-shadow-md bg-primary text-white py-2 px-10 md:px-16 md:ml-2 rounded hover:bg-[#229F8B]"
+        >
+          Procurar
+      </button>
+      <button
+        onClick={handleClean}
+        className="drop-shadow-md bg-primary text-white py-2 px-10 md:px-16 ml-2 mt-3 md:ml-1 md:mt-0 rounded hover:bg-[#229F8B]"
       >
-        Procurar
-    </button>
-    <button
-      onClick={handleClean}
-      className="drop-shadow-md bg-primary text-white py-2 px-16 ml-1 rounded hover:bg-[#229F8B]"
-    >
-      Limpar
-    </button>
+        Limpar
+      </button>
+    </div>
   </div>
   )
 }
