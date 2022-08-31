af4804818951577d95be89d6f410c9480ba48d5e
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-08-31 12:41:49 -0300

    style: estilizando componente de card

--- dev-repo-web/src/components/Card/index.js
+++ dev-repo-web/src/components/Card/index.js
@@ -1,14 +1,26 @@
 import React from "react";
+import clearRepo from '../../imgs/clearRepo.svg';
 
 const Card = ({rep, onDelete}) => {
 
   return(
-    <li className="item bg-primary">
+    <li className="item bg-primary drop-shadow-md p-1 px-5 rounded hover:bg-[#229F8B]">
       <div className="info">
-        <p>{rep.name.split('/')[0]}</p>
-        <p>{rep.name.split('/')[1]}</p>
+        <div className="py-1">
+          <p className="text-white font-medium">Usuário</p>
+          <p className="px-2">{rep.name.split('/')[0]}</p>
+        </div>
+        <div className="py-1">
+          <p className="text-white font-medium">Repo</p>
+          <p className="px-2 ">{rep.name.split('/')[1]}</p>
+        </div>
       </div>
-      <button onClick={() => onDelete(rep.userId,rep._id)}>Apagar</button>
+      <button
+        className="absolute top-3 right-3"
+        onClick={() => onDelete(rep.userId,rep._id)}
+        >
+          <img className="w-[30px]" src={clearRepo} alt="Apagar Repo" />
+      </button>
     </li>
   )
 
