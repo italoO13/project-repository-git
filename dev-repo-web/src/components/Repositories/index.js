86ead835a60414c80c29a6a7a27f311e7891c81e
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-08-31 12:42:19 -0300

    style: alterando logica de responsividade do componente repositorio

--- dev-repo-web/src/components/Repositories/index.js
+++ dev-repo-web/src/components/Repositories/index.js
@@ -4,10 +4,10 @@ import Card from "../Card";
 const Repositories = ({repo, onDelete, loadingError}) => {
 
   return(
-    <div className="repositories container max-w-5xl m-auto px-2 md:px-6">
-    <h2 className="title mt-8 mb-3">Repositórios</h2>
+    <div className="repositories container max-w-5xl m-auto px-2 md:px-6 ">
+    <h2 className="title font-medium text-xl mt-8 mb-3">Repositórios</h2>
 
-    <ul className="list grid grid-cols-2 gap-2">
+    <ul className="list grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-2">
       {
         repo.map((rep) => <Card 
           rep = {rep}
