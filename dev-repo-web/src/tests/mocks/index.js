f3f58a217e23f2beef089ec3fc23c42256ae5009
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-09-01 14:13:46 -0300

    test: mocks iniciais para testar as paginas de login e signup

---
+++ dev-repo-web/src/tests/mocks/index.js
@@ -0,0 +1,16 @@
+export const MOCKUSER = {
+  data: {
+    "user": {
+      "id": "62fd35462f7c01b60898ba0a",
+      "email": "italoiveldo@gmail.com"
+    },
+    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZmQzNTQ2MmY3YzAxYjYwODk4YmEwYSIsImlhdCI6MTY2MjAzOTA2NiwiZXhwIjoxNjYyNjQzODY2fQ.uUK7u8M_aXzTa3uyM9VShqetSJXNOGdheCMFlDxuAJs"
+  }
+}
+
+export const VALIDEMAIL = 'italoiveldo@gmail.com';
+export const INVALIDEMAIL = 'emailInvalido@gmail.com';
+export const NEWEMAIL = 'italoiveldo333@gmail.com'
+
+export const VALIDPASSWORD = 'secret';
+export const INVALIDPASSWORD = 'invalid'
