2c129a01871f3488244953628b1cdd9892fe2ede
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-09-01 14:14:47 -0300

    test: testes para a pagina de login

--- dev-repo-web/src/tests/login.test.js
+++ dev-repo-web/src/tests/login.test.js
@@ -1,12 +1,16 @@
-import React from "react";
 import { screen } from "@testing-library/react";
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from "./utils/renderWithRouter";
+import { MOCKUSER, VALIDEMAIL, VALIDPASSWORD } from "./mocks";
 const mockApi = require('../services/api');
+
 
 describe('Testes que avaliam o funcionamento da pagina de Login', () => {
   beforeEach(() => {
     localStorage.clear();
+  })
+  afterEach(() => {
+    jest.clearAllMocks()
   })
   it('verifica se imagem e titulo são renderizados na tela', () => {
     renderWithRouter('/login');
@@ -37,12 +41,12 @@ describe('Testes que avaliam o funcionamento da pagina de Login', () => {
     const linkSingIn =  screen.getByText('Não tem uma conta?');
     expect(linkSingIn).toBeInTheDocument();
     userEvent.click(linkSingIn)
-    const title = await screen.findByRole('heading', {name: 'Sing up'});
+    const title = await screen.findByRole('heading', {name: 'Sign up'});
     expect(title).toBeInTheDocument();
   })
   it('verifica se caso email e senha sejam incorretos aparece uma mensagem informando "Email ou senha incorretos"', async () => {
     jest.spyOn(mockApi, 'createSession')
-    mockApi.createSession.mockResolvedValue(new Error('401'));
+    mockApi.createSession.mockRejectedValue(new Error('401'));
     renderWithRouter('/login');
     const inputEmail = screen.getByRole('textbox', {name: /email/i})
     const inputPassword = screen.getByLabelText(/password/i)
@@ -51,5 +55,32 @@ describe('Testes que avaliam o funcionamento da pagina de Login', () => {
     userEvent.type(inputPassword, 'senhaInvalida');
     userEvent.click(buttonEntrar);
     const alert = await screen.findByText('Email ou senha inválidos')
+    expect(alert).toBeInTheDocument();
   })
+  it('verifica se caso email e senha sejam válidos o usuário é redirecionado para a rota "/"', async() => {
+    jest.spyOn(mockApi, 'createSession')
+    mockApi.createSession.mockReturnValue(MOCKUSER)
+    renderWithRouter('/login');
+    const inputEmail = screen.getByRole('textbox', {name: /email/i})
+    const inputPassword = screen.getByLabelText(/password/i)
+    const buttonEntrar = screen.getByRole('button', {name:'Entrar'})
+    userEvent.type(inputEmail, VALIDEMAIL);
+    userEvent.type(inputPassword, VALIDPASSWORD);
+    userEvent.click(buttonEntrar);
+    const titleLogo = await screen.findByText('Sistem Repo')
+    expect(titleLogo).toBeInTheDocument();
+  });
+  it('verifica se ocorra um erro no servidor ele retornará o mesmo na tela', async() => {
+    jest.spyOn(mockApi, 'createSession')
+    mockApi.createSession.mockRejectedValue(new Error('Erro no servidor'))
+    renderWithRouter('/login');
+    const inputEmail = screen.getByRole('textbox', {name: /email/i})
+    const inputPassword = screen.getByLabelText(/password/i)
+    const buttonEntrar = screen.getByRole('button', {name:'Entrar'})
+    userEvent.type(inputEmail, VALIDEMAIL);
+    userEvent.type(inputPassword, VALIDPASSWORD);
+    userEvent.click(buttonEntrar);
+    const alert = await screen.findByText('Erro no servidor')
+    expect(alert).toBeInTheDocument();
+  });
 })
