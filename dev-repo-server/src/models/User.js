89bca495c4874780a90106bd062a0883453875fe
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-09-03 17:29:09 -0300

    fix: colocando letra inicial maiuscula quando um usuário é encontrado

--- dev-repo-server/src/models/User.js
+++ dev-repo-server/src/models/User.js
@@ -2,14 +2,14 @@ const User = require('../shemaModels/user.shemadb');
 const CustomError = require('../utils/customError');
 
 const getUserAll = async() => {
-   const users = await User.find();
+   const users = await User.find({}, {password: 0});
    return users;
 }
 
 const createUser = async (email, password) => {
   const user = await User.findOne({email});
   if(user) {
-    throw new CustomError(422, `user ${email} already exists`)
+    throw new CustomError(422, `User ${email} already exists`)
   }
   const newUser = await User.create({email, password});
   return newUser;
