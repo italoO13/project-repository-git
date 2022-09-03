2c16f2a013a4f1c5e1903bb8a86721b6b690fb2e
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-09-03 17:31:19 -0300

    chore: adicionado mocks de usuario e repositorio

--- dev-repo-server/src/tests/mocks/index.js
+++ dev-repo-server/src/tests/mocks/index.js
@@ -1,11 +1,60 @@
-module.exports ={
+module.exports = {
   MOCK_USER : {
     "_id": "62fd35462f7c01b60898ba0a",
     "email": "italoiveldo@gmail.com",
     "password": "secret",
     "createdAt": "2022-08-17T18:36:54.745Z",
     "updatedAt": "2022-08-17T18:36:54.745Z",
-  }
+    "updateOne": () => true,
+    "deleteOne": () => true,
+
+  },
+  MOCK_USER_ALL:[
+    {
+      "_id": "62fd35462f7c01b60898ba0a",
+      "email": "italoiveldo@gmail.com",
+      "createdAt": "2022-08-17T18:36:54.745Z",
+      "updatedAt": "2022-08-17T18:36:54.745Z",
+    }
+  ],
+  MOCK_REPO:{
+    "name": "italoO13/project-repositgory-ggit",
+    "url": "https://github.com/itagloO13/project-repository-git",
+    "userId": "62fd35462f7c01b60898ba0a",
+    "_id": "63019550798af340971e541d",
+    "createdAt": "2022-08-21T02:15:44.050Z",
+    "updatedAt": "2022-08-21T02:15:44.050Z",
+    "__v": 0
+  },
+  MOCK_REPO_ALL: [
+    {
+      "_id": "6302465452ffbe6d5de5162d",
+      "name": "italoO13/desafio-pratico-elcoma",
+      "url": "https://github.com/italoO13/desafio-pratico-elcoma",
+      "userId": "62fd35462f7c01b60898ba0a",
+      "createdAt": "2022-08-21T14:51:00.729Z",
+      "updatedAt": "2022-08-21T14:51:00.729Z",
+      "__v": 0
+    },
+    {
+      "_id": "630e027e599d8050b80418af",
+      "name": "italoO13/teste",
+      "url": "https://github.com/italoO13/teste",
+      "userId": "62fd35462f7c01b60898ba0a",
+      "createdAt": "2022-08-30T12:28:46.855Z",
+      "updatedAt": "2022-08-30T12:28:46.855Z",
+      "__v": 0
+    },
+    {
+      "_id": "630f6c57d1a75a66ff1cfe19",
+      "name": "italoO13/testeagoravaiasdasd",
+      "url": "https://github.com/italoO13/testeagoravaiasdasd",
+      "userId": "62fd35462f7c01b60898ba0a",
+      "createdAt": "2022-08-31T14:12:39.492Z",
+      "updatedAt": "2022-08-31T14:12:39.492Z",
+      "__v": 0
+    }
+  ]
 
 }
 
